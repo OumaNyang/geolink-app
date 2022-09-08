50522d20646e045c10c2cd7bdd0cf586c550ea58
Milton Ouma <ouma.nyang1@gmail.com>
2022-09-08 14:32:41 +0300

    updated applicationn controller

---
+++ geolink-server/app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < Sinatra::Base
+    set default_content_type: "application/json"
+    
+    get '/counties' do
+      counties = County.all
+      counties.to_json
+    end
+    
+    get '/counties/:id' do
+      bakery = County.find(params[:id])
+      bakery.to_json(include: :constituencies)
+    end
+   
+  end
+  
