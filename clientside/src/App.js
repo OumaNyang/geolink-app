56786ebbc262abd2dce58985af8783cba9270208
Milton Ouma <ouma.nyang1@gmail.com>
2022-09-10 11:21:48 +0300

    done clientside

---
+++ clientside/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css'
+
+export default function App() {
+ return (
+    <div className="App">
+      <div className="container">
+        <div className="card">
+            <div className="card-body">
+                <h4>This is geo link API</h4>
+            </div>
+        </div>
+        </div>
+    </div>
+  );
+}
