e1a0837e784b90f921608731353c0eff4d81a0b2
Milton Ouma <ouma.nyang1@gmail.com>
2022-09-10 10:40:24 +0300

    Done serverside modules

--- geolink-server/app/controllers/application_controller.rb
+++ geolink-server/app/controllers/application_controller.rb
@@ -1,14 +1,88 @@
 class ApplicationController < Sinatra::Base
-    set default_content_type: "application/json"
-    
-    get '/counties' do
+  set :default_content_type, 'application/json'
+
+  # Add your routes here
+  get "/" do
+    { message: "Good luck with your project!" }.to_json
+  end
+  # Post county
+  post '/counties' do
+    county = County.create(
+      county_code: params[:county_code],
+      county_name: params[:county_name]
+     )
+    county.to_json
+  end
+  # Patch county
+  patch  '/counties' do
+  county = County.find(params[:id])
+  county.update(
+    county_code: params[:county_code], 
+    county_name: params[:county_name]
+  )
+  counties = County.all
+  counties.to_json
+end 
+
+   # Delete county by id
+    delete '/counties/:id' do 
+      county = County.find(params[:id])
+      county.destroy 
       counties = County.all
       counties.to_json
+ end 
+  # get all counties
+  get "/counties" do
+    counties = County.all
+    counties.to_json
+  end 
+  # get county by id
+  get "/counties/:id" do
+    county = County.find(params[:id])
+    county.to_json
+  end
+    # get county by county code
+    get "/counties/:by_code" do
+      counties = County.find_by(params[:county_code])
+      counties.to_json
     end
-    
-    get '/counties/:id' do
-      bakery = County.find(params[:id])
-      bakery.to_json(include: :constituencies)
+
+   # Post constituency
+    post '/constituencies' do
+    constituency = Constituency.create(
+      constituency_code: params[:constituency_code],
+      constituency_name: params[:constituency_name],
+      county_code: params[:county_code]
+     )
+     constituency.to_json
+  end
+  # Patch constituency
+  patch  '/constituencies' do
+    constituency = Constituency.find(params[:id])
+    constituency.update(
+      constituency_code: params[:constituency_code],
+      constituency_name: params[:constituency_name],
+      county_code: params[:county_code]
+  )
+  constituencies = Constituency.all
+  constituencies.to_json
+end 
+
+   # Delete constituency by id
+    delete '/constituencies/:id' do 
+      constituency = Constituency.find(params[:id])
+      constituency.destroy 
+      constituencies = Constituency.all
+      constituencies.to_json
+ end 
+  # get all constituencies
+  get "/constituencies" do
+    constituencies = Constituency.all
+    constituencies.to_json
+  end
+    # get constituency by id
+    get "/constituencies/:id" do
+      constituency = Constituency.find(params[:id])
+      constituency.to_json
     end
- end
-  
+end
--- geolink-server/db/schema.rb
+++ geolink-server/db/schema.rb
@@ -10,6 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_09_09_113405) do
+
+  create_table "constituencies", force: :cascade do |t|
+    t.string "constituency_code"
+    t.string "constituency_name"
+    t.string "county_code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "counties", force: :cascade do |t|
+    t.string "county_code"
+    t.string "county_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
