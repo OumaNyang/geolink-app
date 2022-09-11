3a8fcfdf3fd618fbc8ec99f41f5eb061d34e8fd0
Milton Ouma <ouma.nyang1@gmail.com>
2022-09-11 11:51:31 +0300

    Constituencies component CRUD

--- clientside/src/components/Constituencies.js
+++ clientside/src/components/Constituencies.js
@@ -1,7 +1,38 @@
 import React from 'react'
 import {Route} from 'react-router-dom'
+import {useEffect, useState} from 'react'
+import Constituency from './Constituency.js'
 
 const Constituencies = () => {
+const [constituencies, setConstituencies] = useState([])
+
+  useEffect(() => {
+    const getConstituencies = async () => {
+        const constituenciesFromServer = await fetchConstituencies()
+        setConstituencies(constituenciesFromServer)
+    }
+    getConstituencies()
+}, [])
+
+    // Fetch Constituencies records 
+    const fetchConstituencies = async () => {
+      const res = await fetch('http://localhost:9292/constituencies')
+      const data = await res.json()
+      return data
+  }
+
+      // Delete Constituency record
+     const deleteConstituency = async (id) => {
+      const res = await fetch(`http://localhost:9292/constituencies/${id}`, {
+          method: 'DELETE',
+      })
+      //We should control the response status to decide if we will change the state or not.
+      res.status === 200
+          ? setConstituencies(constituencies.filter((constituency) => constituency.id !== id))
+          : alert('Error deleting consituency record')
+  }
+
+
   return (
     <div className='about'>
       <h4>Kenya Constituencies</h4>
@@ -11,7 +42,30 @@ path='/constituencies'
 exact
 render={(props) => (
 <>
- No constituency records found
+{constituencies.length > 0 ? (
+ 
+ <table>
+   <thead>
+            <th>Const. code </th>
+            <th>Const. name</th>
+            <th>County</th>
+            <th>Action</th>
+     </thead>
+     <tbody>
+     {constituencies.map((constituency, index) => (
+        <Constituency key={index} constituency={constituency}  onDelete={deleteConstituency} />
+      ))}
+       </tbody>
+       </table>
+
+) : (
+  'No constituency records found'
+)}
+
+
+
+
+
 </>
 )}
 />
---
+++ clientside/src/components/Constituency.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { FaTimes } from 'react-icons/fa'
+// import { default as NumberFormat } from 'react-number-format';
+
+const Constituency = ({ constituency, onDelete, onToggle }) => {
+  return (
+<tr>
+<td>{constituency.constituency_code}</td>
+<td>{constituency.constituency_name}</td>
+<td>{constituency.county_code}</td>
+<td>
+<button   className="btn-delete" onClick={() => {if(window.confirm(`Delete ${constituency.constituency_name} 
+constituency from records? Note that all constituencies will also be deleted!`)){
+onDelete(constituency.id)
+};}}>
+<FaTimes  style={{ color: 'white', cursor: 'pointer' }}/>  Delete</button>
+</td>
+</tr>
+)
+}
+export default Constituency
